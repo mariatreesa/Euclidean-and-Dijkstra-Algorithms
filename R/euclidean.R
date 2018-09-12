5aeb5326bf934a8e711c4ff339be1b212dcd18d8
unknown <obhutara@gmail.com>
2018-09-12 23:13:09 +0200

    euclidean changed with repeat

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,11 +1,20 @@
 # LAB3
-# Authors: Maria Treesa Sebastian(marse306), Brian Masinde, Omkar
+# Authors: Maria Treesa Sebastian(marse306), Brian Masinde, Omkar(omkbh878)
 # Greatest commmon divisor
-euclidean <- function(a,b){
-  while(b != 0){
-    t <- b
-    b <- a %% b
-    a <- t
-  }
-  return(a)
+euclidean <- function(x,y){
+  stopifnot(is.numeric(x) || is.numeric(y))
+  i<-1
+    repeat{
+      temp<-y
+      y<- x%%y
+      x<-temp
+      if(y==0) break
+    }
+  return(x)
 }
+
+euclidean(123612, 13892347912)
+#[1] 4
+euclidean(100, 1000)
+#[1] 100
+
