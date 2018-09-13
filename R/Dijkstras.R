9e02d8f3dbd5779aaf25fbd1089bb4b7351e0198
Maria Treesa Sebastian <mariatreesaa@gmail.com>
2018-09-13 22:36:07 +0200

    changesto R files

--- R/Dijkstras.R
+++ R/Dijkstras.R
@@ -3,11 +3,11 @@
 #' @param graph as dataframe
 #' @param init_node as numeric
 #'
-#' @return Shortest distance to all vertex from init_node
+#' @return Shortest distance to all vertex from init_node . The wikipage link to algorithm : <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
 #'
 #' @export
 #'
-#' @examples dijkstra(dataframe,1)
+#' @examples dijkstra(graph = data.frame(v1=c(1,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6,6,6),v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9)),init_node = 1)
 #'
 #'
 dijkstra <- function(graph, init_node){
