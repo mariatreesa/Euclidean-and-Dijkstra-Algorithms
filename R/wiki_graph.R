d9b5b9e56d5a31b529761a855f12dc4855da76ce
Maria Treesa Sebastian <mariatreesaa@gmail.com>
2018-09-14 20:30:27 +0200

    updated dataset documentation

--- R/wiki_graph.R
+++ R/wiki_graph.R
@@ -9,5 +9,5 @@
 #'   \item{w}{weight of the path taken}
 #'   ...
 #' }
-#' @source \url{https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm}
+#' @source <https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm>
 "wiki_graph"
