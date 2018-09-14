b3c49035e54c8e13c1ab74f0216a1579d03fbfd7
Maria Treesa Sebastian <mariatreesaa@gmail.com>
2018-09-14 20:38:24 +0200

    updated wiki graph documentation

--- R/wiki_graph.R
+++ R/wiki_graph.R
@@ -2,12 +2,11 @@
 #'
 #' A dataset containing the weights attached to traversing from sources to destinations.
 #'
-#' @format A data frame with 18 rows and 3 variables:
+#' @format A data frame with 18 rows and 3 variables
 #' \describe{
 #'   \item{v1}{id of the source vertices}
 #'   \item{v2}{id of the destination vertices}
 #'   \item{w}{weight of the path taken}
-#'   ...
 #' }
-#' @source <https://en.wikipedia.org/wiki/Dijkstra\%27s_algorithm>
+#' @source <https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm>
 "wiki_graph"
