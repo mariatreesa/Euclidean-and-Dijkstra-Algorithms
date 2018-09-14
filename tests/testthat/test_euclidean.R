e232b41414b454991757837f95cb6eacab741c0e
unknown <obhutara@gmail.com>
2018-09-14 21:05:09 +0200

    added test for euclidean

--- tests/testthat/test_euclidean.R
+++ tests/testthat/test_euclidean.R
@@ -1,12 +1,15 @@
 # test for the function euclidean()
 context("euclidean")
 
-test_that("User Input Validation",{
- expect_error(euclidean("b", 100))
-}
-)
+test_that("GDC is calculated correctly.", {
+  expect_equal(euclidean(123612, 13892347912), 4)
+  expect_equal(euclidean(100, 1000), 100)
+  expect_equal(euclidean(-100, 1000), 100)
+})
 
-test_that("Function Validation",{
-	expect_equal(euclidean(100,1000),100)
-}
-)
+
+test_that("Wrong input throws an error.", {
+  expect_error(euclidean("100", 1000))
+  expect_error(euclidean(100, "1000"))
+  expect_error(euclidean(TRUE, "1000"))
+})
